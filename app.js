f5e25ea04adcd8012ff4c3c0fdd9c67ca20c51e1
311 <segara968@gmail.com>
2022-03-23 12:35:21 -0500

    firts commit

---
+++ app.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const dotenv = require('dotenv');
+dotenv.config({ path: './env/.env' });
+
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+const bcryptjs = require('bcryptjs');
+
+const session = require('express-session');
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+const connection = require('./database/db');
+const res = require('express/lib/response');
+
+app.get('/', (req, res) => {
+    res.render('index', { msg: 'Yeferson shut up' });
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+app.post('/register', async (req, res) => {
+    const user = req.body.user;
+    const name = req.body.name;
+    const rol = req.body.rol;
+    const pass = req.body.pass;
+    let passwordHaash = await bcryptjs.hash(pass, 8);
+    connection.query('INSERT INTO users SET ?', { user: user, name: name, rol: rol, pass: passwordHaash }, async (error, result) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.render('register', {
+                alert: true,
+                alertTitle: "Registration",
+                alertMessage: "Registro correcto",
+                alertIcon: 'success',
+                showConfirmButton: false,
+                timer: 1500,
+                ruta: ''
+            })
+        }
+    })
+})
+
+app.post('/auth', async (req, res) => {
+    const user = req.body.user;
+    const pass = req.body.pass;
+    let passwordHaash = await bcryptjs.hash(pass, 8);
+    if (user && pass) {
+        connection.query('SELECT * FROM users WHERE user = ?', [user], async (error, result) => {
+            if (result.length == 0 || !(await bcryptjs.compare(pass, result[0].pass))) {
+                res.render('login', {
+                    alert: true,
+                    alertTitle: 'Error',
+                    alertMessage: "Usuario y/o pasword incorrecto",
+                    alertIcon: "error",
+                    showConfirmButton: true,
+                    timer: 1500,
+                    ruta: 'login'
+                });
+            } else {
+                req.session.name = result[0].name
+                res.render('login', {
+                    alert: true,
+                    alertTitle: "Conexión exitosa",
+                    alertMessage: "Login correcto",
+                    alertIcon: "success",
+                    showConfirmButton: true,
+                    timer: 1500,
+                    ruta: ''
+                })   
+            }
+        })     
+
+    }else{
+        res.render('login', {
+            alert: true,
+            alertTitle: "Advertencia",
+            alertMessage: "por favor ingrese un usuiario y/o password",
+            alertIcon: "warning",
+            showConfirmButton: true,
+            timer: 1500,
+            ruta: 'login'
+        })
+    }
+})
+
+app.listen(3000, (req, res) => {
+    console.log('Mierda está corriendo aquí http://localhost:3000');
+})
---
+++ database/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+connection.connect((error)=>{
+    if(error){
+        console.log('El error eres tu, conexión: '+error);
+        return;
+    }
+    console.log('Estas conectado, ¡que bien!');
+});
+module.exports = connection;
